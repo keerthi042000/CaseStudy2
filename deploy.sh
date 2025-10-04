8deee530f723074522e69c67235b1cdd5c5a22ed
Keerthi Vemula Murali <kmurali@wpi.edu>
2025-10-04 17:04:20 -0400

    Deployment changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+HUGGINGFACE_TOKEN=$1
+
+if [ -z "$HUGGINGFACE_TOKEN" ]; then
+    echo "Error: Please provide your Hugging Face token as the first argument."
+    echo "Usage: ./deploy.sh <HUGGINGFACE_TOKEN>"
+    exit 1
+fi
+
 sudo apt update
 # sudo apt install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxrender1 libxext6 ffmpeg
 sudo apt install -y libgl1-mesa-glx
@@ -13,7 +21,9 @@ fi
 
 cd ~/projects
 python3 -m venv CaseStudy2_venv
-source python3 -m venv CaseStudy2_venv /bin/activate
+source python3 -m venv CaseStudy2_venv/bin/activate
+
+export HF_TOKEN="$HUGGINGFACE_TOKEN"
 
 pip install --upgrade pip
 pip install -r ~/projects/CaseStudy2/requirements.txt
